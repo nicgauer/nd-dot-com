bdbd4fbafea55e1bdf918e1744555fad2ea654eb
nic <nicgauer@gmail.com>
2025-12-06 19:05:39 -0600

    fix: fix linter errors causing build failures

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <h1>neighbor dog</h1>
         <h3>Bark Bark</h3>
-        <iframe style="border: 0; width: 350px; height: 442px;" src="https://bandcamp.com/EmbeddedPlayer/track=547416725/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/transparent=true/" seamless><a href="https://neighbordogmn.bandcamp.com/track/cat-song">Cat Song by Neighbor Dog</a></iframe>
+        <iframe title="Cat Song by Neighbor Dog - Bandcamp Player" style={{ border: 0, width: '350px', height: '442px' }} src="https://bandcamp.com/EmbeddedPlayer/track=547416725/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/transparent=true/" seamless><a href="https://neighbordogmn.bandcamp.com/track/cat-song">Cat Song by Neighbor Dog</a></iframe>
       </header>
     </div>
   );
